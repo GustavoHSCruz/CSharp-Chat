efa5174bca30c69afb0033766b61a2df3eccc02d
Gustavo Cruz <gustavo_cruz08@hotmail.com>
2025-03-10 23:15:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var port = GeneratePort();
+
+        var tcpListener = new TcpListener(IPAddress.Any, port);
+
+        tcpListener.Start();
+
+        Console.WriteLine($"Server has Started at port: {port}");
+
+        Console.ReadLine();
+    }
+
+    private static int GeneratePort()
+    {
+        return (int)new Random().NextInt64(5000, 10000);
+    }
+}
